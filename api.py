aa3ffabedc40a29404045dbd609f382b8b6da75c
mozaldy <moxaldy@gmail.com>
2024-01-18 05:29:28 +0700

    initialize vue

--- api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-query = input('Query for anime: ')
-
-response = requests.get(f'https://api.jikan.moe/v4/anime?q={query}').json()
-for data in response['data']:
-    print(data['title'])
