1724d43c0a204322475b325f67ebb24d6087d6fb
mozaldy <moxaldy@gmail.com>
2024-01-20 18:05:33 +0700

    deploy 3

--- assets/HomeView-bFaZPpwz.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";import{o,c}from"./index-wdNgxpW7.js";const r={};function t(n,s){return o(),c("h1",null,"This is the home page!")}const m=e(r,[["render",t]]);export{m as default};
