bb65d11abc8641e1dd6b2d51eaaf003e1f8672ef
mozaldy <moxaldy@gmail.com>
2024-01-20 17:48:21 +0700

    deploy 2 -- lupa vite.config.js wwkkwk

---
+++ assets/AnimeView-g4p1dPku.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";import{o as c,c as n}from"./index-wdNgxpW7.js";const o={};function r(t,a){return c(),n("h1",null,"This is a anime page!")}const _=e(o,[["render",r]]);export{_ as default};
