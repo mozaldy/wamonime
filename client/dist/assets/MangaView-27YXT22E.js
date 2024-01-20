1a427ce7af10bb99a94554c3d9c636bcb1d13998
mozaldy <moxaldy@gmail.com>
2024-01-20 17:48:21 +0700

    deploy 2 -- lupa vite.config.js wwkkwk

--- client/dist/assets/MangaView-27YXT22E.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as n}from"./index-Tf0J6x0L.js";const c={};function o(r,t){return a(),n("h1",null,"This is an Manga page!")}const f=e(c,[["render",o]]);export{f as default};
